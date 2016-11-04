e8dfa46915f4b8d1965e2226f9bf5ffeafbcff51
523225812@qq.com <523225812@qq.com>
2016-11-04 20:01:03 +0800

    new

--- todolist.js
+++ todolist.js
@@ -5,15 +5,23 @@
       $(this).children('i').toggleClass('showV');
     })
 
+    // 点击X 删除事项
+    $('#content').on('click','.content_list>i',function(){
+      $(this).closest('.content_list').remove();
+      num=$('.content_list').length;
+      console.log(num);
+    })
+
     // 打钩部分
     $('#content').on('click','.circle',
       function(){
         if($(this).find('i').hasClass('showV')){
-          $(this).find('i').removeClass('showV done');
+          $(this).find('i').removeClass('showV');
+          $(this).closest('.content_list').removeClass('done');
           $(this).siblings('.content_text').removeClass('fontDel');
-
         }else{
-          $(this).find('i').addClass('showV done');
+          $(this).find('i').addClass('showV');
+          $(this).closest('.content_list').addClass('done');
           $(this).siblings('.content_text').addClass('fontDel');
         }
     })
@@ -21,25 +29,53 @@
     // 首部展示收缩事情
     $('#container_input span').on('click',function(){
       $('#content').toggleClass('active');
+      $('.content_list').removeClass('hide');
       if($('#content').hasClass('active')){
         $('.btn_do').addClass('selected');
+        $('.btn_do').siblings().removeClass('selected');
+
       }else{
         $('.btn_do').removeClass('selected');
+        $('.btn_do').siblings().removeClass('selected');
       }
     })
 
     // 底部展示全部事项
     $('.btn_do').on('click',function(){
       $('#content').addClass('active');
+      $('.content_list').removeClass('hide');
       $(this).addClass('selected');
       $(this).siblings().removeClass('selected');
+    });
+
+    // 底部展示全部事项
+    $('.btn_done').on('click',function(){
+      $(this).addClass('selected');
+      $(this).siblings().removeClass('selected');
+      $('.content_list').each(function(){
+        if(!$(this).hasClass('done')){
+          $(this).addClass('hide');
+        }
+      })
     })
+
+    // 底部删除完成事项
+    $('.btn_clear').on('click',function(){
+      $('.content_list').each(function(){
+        console.log($(this));
+        if($(this).hasClass('done')){
+          $(this).remove();
+          num=$('.content_list').length;
+          console.log(num);
+        }
+      });
+    });
 
     // 添加事项
     $(document).ready(function(){
       $('.input_box').on('keyup',function(e){
-          if(e.keyCode==13){
-            var val_text=$(this).val();
+          var val_text=$(this).val();
+          if(e.keyCode==13&&(val_text!=='')){
             var newList='',
                 $nodes;
             newList += '<div class="content_list">';
@@ -54,8 +90,23 @@
             $('.input_box').val('');
             $('#content').addClass('active');
             $('.btn_do').addClass('selected');
+            $('.btn_do').siblings().removeClass('selected');
+            num=$('.content_list').length;
+            console.log(num);
+            localStorage.setItem('things',val_text)
+            // document.write(localStorage.things);
           }
         })
       })
+      // // 待办事项数字计算
+      // $(document).on('click keyup',function(e){
+      //   if(e.keyCode==13&&(val_text!=='')){
+      //     num=$('.content_list').length;
+      //   }
+      //
 
-      // 待办事项数字计算
+
+      //
+      // })
+
+      var num;
